ed014b954f45e3a3bff943779fdda1d97578e9a2
Serdal Kepil <skepil@ptc.com>
2023-11-21 23:54:18 +0300

    Upgrade SAM CLI cmds

--- sam/java-crud-api-with-layer/install-java-maven.sh
+++ sam/java-crud-api-with-layer/install-java-maven.sh
@@ -25,3 +25,9 @@ sam build
 sam deploy --guided
 
 
+# Upgrade SAM CLI
+
+wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install --update
+sam --version
